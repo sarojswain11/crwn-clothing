820dc52c04c30d35b77a8556bf5af14c85474ea0
Saroj Swain <sarojkumarswain51@gmail.com>
2020-05-04 19:00:33 +0530

    Updates

---
+++ precache-manifest.36ae627d933a86449816d0538a2d3065.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b436ec0d57491e86db65d00781d00c0",
+    "url": "/crwn-clothing/index.html"
+  },
+  {
+    "revision": "532dac2d51fc90c51ca7",
+    "url": "/crwn-clothing/static/css/main.5df6a31c.chunk.css"
+  },
+  {
+    "revision": "23cdccfc5143027e6af6",
+    "url": "/crwn-clothing/static/js/2.8757fc18.chunk.js"
+  },
+  {
+    "revision": "532dac2d51fc90c51ca7",
+    "url": "/crwn-clothing/static/js/main.1122c61a.chunk.js"
+  },
+  {
+    "revision": "f93477dbf1f75a56f892",
+    "url": "/crwn-clothing/static/js/runtime~main.e91a80aa.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/crwn-clothing/static/media/crown.ed28accb.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/crwn-clothing/static/media/shopping-bag.30f07071.svg"
+  }
+]);
